4884cd96846badbacc85df724e717af3121d6f8c
shurtarte5 <hurtartesergio@gmail.com>
2025-11-28 21:06:18 -0600

    Refactors movie data fetching for simplicity
    Simplifies the movie data fetching process by removing the paging library and related components.
    
    This change streamlines the data flow, making it easier to manage and display the movie list. It replaces the previous paged approach with a simple list of movies fetched directly from the API. This also involves removing video details and related use cases, focusing on the basic movie listing functionality.

--- app/src/main/java/com/sergio/sergiodevhub/data/network/TmdbApiService.kt
+++ app/src/main/java/com/sergio/sergiodevhub/data/network/TmdbApiService.kt
@@ -1,10 +1,7 @@
 package com.sergio.sergiodevhub.data.network
 
 import com.sergio.sergiodevhub.data.dto.MovieResponseDto
-import com.sergio.sergiodevhub.data.dto.VideoResponseDto
-
 import retrofit2.http.GET
-import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface TmdbApiService {
@@ -12,12 +9,4 @@ interface TmdbApiService {
     suspend fun getPopularMovies(
         @Query("page") page: Int = 1
     ): MovieResponseDto
-    
-    @GET("movie/{movie_id}/videos")
-    suspend fun getMovieVideos(
-        @Path("movie_id") movieId: Int
-    ): VideoResponseDto
 }
-
-
-
